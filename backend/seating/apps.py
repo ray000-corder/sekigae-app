680c9de6bb6a4d5bad87c7e48dbd416e6964b65b
Rei <yama.game0719@gmail.com>
2025-06-18 12:57:19 +0900

    Implement signal to auto-create seat layout for new users

--- backend/seating/apps.py
+++ backend/seating/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SeatingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'seating'
+
+    def ready(self):
+        import seating.signals # signals.pyをインポート
---
+++ backend/seating/migrations/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import SeatLayout
+
+@receiver(post_save, sender=User)
+def create_default_seat_layout(sender, instance, created, **kwargs):
+    """
+    新しいユーザーが作成された(created=True)場合に、この関数が自動的に呼び出される
+    """
+    if created:
+        # 新しいユーザーに、デフォルトの座席表を作成する
+        SeatLayout.objects.create(user=instance, name=f"{instance.username}の座席表")
+        print(f"Created default seat layout for new user: {instance.username}")
